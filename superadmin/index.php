90a815044931055e98c1467183d139d22a357814
iina <rnnoleal@gmail.com>
2025-04-23 16:00:07 +0800

    Pasted Template

--- superadmin/index.php
+++ superadmin/index.php
@@ -30,40 +30,109 @@ require("startsession.php");
             include("navbar.php");
             ?>
             <!-- information container -->
-            <div class="container-fluid p-3">
-                <div class="bg-light bg-opacity-25 rounded p-3 shadow">
-                    <?php
-                    if (isset($_SESSION['saID'])) {
-                        echo '<h1 class="text-center display-6 mx-auto">Welcome to Pestastic Official Inventory ' . $_SESSION['saUsn'] . '</h1>';
 
-                    } else {
-                        echo "<h1 class='display-6 mx-auto text-center bg-light rounded bg-opacity-25'>Access Denied. No user is signed.</h1>";
-                    }
 
-                    ?>
+            <div class="row m-2 gap-2">
+                <div class="col-5 bg-light bg-opacity-25 border rounded p-3 shadow">
+                    <h1 class=" display-6 mx-auto">Welcome back Manager <strong style="text-transform: capitalize;">
+                                <?= $_SESSION['saUsn'] ?></strong>
+                    </h1>
+                    <p class="fs-3"></p>
+                </div>
+                <div class="col bg-light bg-opacity-25 border rounded p-3 shadow">
+                    <div>
+                        <canvas id="transPie" width="250" height="
+                        250"></canvas>
+                    </div>
+                </div>
+                <div class="col bg-light bg-opacity-25 border rounded p-3 shadow">
+                    <p class="text-center align-text-center fs-3 mx-auto"><i class="bi bi-exclamation-circle"></i> Pending Transactions</p>
+                    <table class="table-hover rounded overflow-hidden table">
+                        <caption class="mt-auto">Check your <a href="transactions.php"
+                                class=" link-underline-opacity-0 link-underline link-body-emphasis link-underline-opacity-0-hover">transactions</a>
+                            for more.</caption>
+                        <thead>
+                            <tr class="text-center">
+                                <th scope="col">ID</th>
+                                <th scope="col">Customer</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody id="pendingtrans" class=""></tbody>
+                    </table>
+                </div>
 
+            </div>
+
+            <div class="row m-2 gap-2">
+
+                <div class="col bg-light bg-opacity-25 border rounded p-3 shadow">
+                    <p class="text-center fs-3 mx-auto">Pending Chemical Requests</p>
+                    <table class="table-hover rounded overflow-hidden table">
+                        <caption>Recently requested addition of chemicals. Check <a href="inventory.php"
+                                class=" link-underline-opacity-0 link-underline link-body-emphasis link-underline-opacity-0-hover">inventory</a>
+                            page for more.</caption>
+                        <thead>
+                            <tr class="text-center">
+                                <th scope="col">Name</th>
+                                <th scope="col">Brand</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody id="pendingchem" class=""></tbody>
+                    </table>
+                </div>
+                <div class="col bg-light bg-opacity-25 border rounded p-3 shadow">
+                    <p class="text-center fs-3 mx-auto">Low Stock Chemicals</p>
+                    <table class="table-hover rounded overflow-hidden table">
+                        <caption>Check <a href="inventory.php?chem=low"
+                                class=" link-underline-opacity-0 link-underline link-body-emphasis link-underline-opacity-0-hover">inventory</a>
+                            to see all chemicals low in stock.</caption>
+                        <thead>
+                            <tr class="text-center">
+                                <th scope="col">Name</th>
+                                <th scope="col">Brand</th>
+                                <th scope="col">Level</th>
+                            </tr>
+                        </thead>
+                        <tbody id="lowchemicals"></tbody>
+                    </table>
+                </div>
+                <div class="col bg-light bg-opacity-25 border rounded p-3 shadow">
+                    <p class="text-center fs-3 mx-auto">Available Equipments</p>
+                    <table class="table-hover rounded overflow-hidden table">
+                        <caption>Check your <a href="equipments.php"
+                                class=" link-underline-opacity-0 link-underline link-body-emphasis link-underline-opacity-0-hover">equipments</a>
+                            for more.</caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">Equipment</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>John</td>
+                                <td>@social</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="container text-center">
-                <div class="row g-2 m-2">
-                    <div class="bg-light bg-opacity-25 rounded p-3 shadow container col-sm">
-                        <h1 class="text-center text-wrap display-6">Items Stock</h1>
-                    </div>
-                    <div class="bg-light bg-opacity-25 rounded p-3 shadow container col-sm">
-                        <h1 class="text-center text-wrap display-6">Available Equipments</h1>
-                    </div>
-                    <div class="bg-light bg-opacity-25 rounded p-3 shadow container col-sm">
-                        <h1 class="text-center text-wrap display-6">Function</h1>
-                    </div>
-                </div>
-                <div class="row g-2">
-                    <div class="bg-light bg-opacity-25 rounded p-3 shadow container col-sm">
-                        <h1 class="text-center text-wrap display-6">Available Equipments</h1>
-                    </div>
-                </div>
-            </div>
+        </main>
     </div>
-    </main>
 
 
     <?php
