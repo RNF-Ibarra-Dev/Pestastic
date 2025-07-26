def87b8eb7177669829c2dc161e96467c10956ff
iina <rnnoleal@gmail.com>
2025-07-26 15:51:22 +0800

    Refactor session cookie parameters: Uncomment session_set_cookie_params for improved session security configuration

--- superadmin/startsession.php
+++ superadmin/startsession.php
@@ -3,14 +3,14 @@
 ini_set("session.use_only_cookies", "1");
 ini_set("session.use_strict_mode", "1");
 
-// session_set_cookie_params([
-//     'lifetime' => 1800,
-//     // use website url when up
-//     'domain' => 'localhost',
-//     'path' => '/',
-//     'secure' => true,
-//     'httponly' => true,
-// ]);
+session_set_cookie_params([
+    'lifetime' => 1800,
+    // use website url when up
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true,
+]);
 
 session_start();
 
