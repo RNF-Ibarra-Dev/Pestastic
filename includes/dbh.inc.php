d8fa82179940bc27d80050292fcb006a1968331a
iina <rnnoleal@gmail.com>
2025-09-09 23:22:14 +0800

    hosting pass

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,16 @@
 <?php
 
 // localhost
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "pestastic_db";
+// $servername = "localhost";
+// $dBUsername = "root";
+// $dBPassword = "";
+// $dBName = "pestastic_db";
 
 // hosting
-// $servername = "localhost";
-// $dBUsername = "u877186800_pestastic_inv";
-// $dBPassword = "Pestastic_inv1";
-// $dBName = "u877186800_pestastic_db";
+$servername = "localhost";
+$dBUsername = "u877186800_pestastic_inv";
+$dBPassword = "Pestastic_inv1";
+$dBName = "u877186800_pestastic_db";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName); 
 
